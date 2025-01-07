f9dc6e9ffa7069af62d460109b26f2544354c1cc
Priyanka Mehta <priyankamehta@Priyankas-MacBook-Air.local>
2025-01-07 20:17:20 +0530

    7jan2025 secondcommit

---
+++ 1firstprogram.cpp
@@ -0,0 +1,37 @@
+//BOILER PLATE CODE
+
+#include <iostream> //we have to include isstream for excute cout
+using namespace std; //use std
+// we can use also std::cout//standard
+int main(){ //main function
+    cout << "Hello from c++" << endl;//print the output  
+    //%it came when we didn't use endl
+    //endl =>end line  "Hello from \n c++" also used it is faster
+   cout <<"Priyanka \n Mehta" << endl;
+   int age=88;
+   cout << sizeof(age) <<endl; //sizeof get the length of variable
+   float PI= 3.14f; //f for float if not considered double
+
+//implicit type casting
+   char grade='A';
+   int value = grade;//in this right value store in value
+   cout << value << endl; //65 //ASCII VALUE
+//explicit type casting
+   double price = 100.99;
+   int newPrice = (int)price;
+   cout << newPrice << endl;
+
+// cin,cout //global objects
+   //User Input 
+   int data;
+   cout << "Enter your data" << endl;
+   cin >> data;
+    cout << "Enter your data " << data;
+    return 0;//return integer
+
+}
+
+//first comiler =>code to compiler//to see any error is coming
+//only excutable file will understable by computer
+
+// line COMMENT
---
+++ 2variables.cpp
@@ -0,0 +1,31 @@
+//a=25 a is variable it is container where we stare our variable
+// we can store single quote a chararter and double for strings
+
+// we can store any datatypes 
+// we can use idetifiers for variables name
+
+//data-types //should not start with numbers or special characters(@#$)
+
+//primitive types
+// integer(int)   => 4 byte (32bit) e.g. 1,3,44 etc
+// charater(char) => 1 byte 'a' ,'b','M etc //ASCII value stores like A=> 65 a=> 97  
+// float(float)   => 4 byte 1.0 ,100.8
+// boolean(bool)  => 1 byte e.g true =>1,false=>0 
+// double(double) => 8 byte 1.11
+
+
+// Type-casting=>Converting data from one type to another
+
+// 1) conversion [implicit]//automatically
+//small to big like float to double (4 byte to 8 byte) ,char to int(1 byte to 4 byte)
+// 2) casting [explicit]//manually by programmer
+//big to small like double to integer (8 byte to 4 byte)
+
+//100.999 never by 101 it only 100 in any conditions
+
+
+//operators 
+
+// 1) Arithmetic (+,-,*,/,%)
+// 2) Relational (==,!=)
+// 3) Logical (&&,||)
